73ac641c76a35a23122d13249bbba4f10adf8d38
curry7 <1599920766@qq.com>
2021-08-21 16:34:28 +0800

    disp的运用

--- matlab8_8.m
+++ matlab8_8.m
@@ -1,8 +1,7 @@
 num = input('Please enther a number (1~100): ');
 
 if  (2 <= num) && (num <= 100);
-    num = num^2;
-    fprintf('num :%g \n',num);
+   disp(num * num);
 else
     disp('Wrong Number');
 end
